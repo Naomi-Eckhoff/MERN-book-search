45dd6d582fd32d142d3059bc5da45b74644e0d38
Naomi-Eckhoff <nsuibhne@gmail.com>
2022-02-20 07:31:37 -0600

    added save book query

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -25,3 +25,22 @@ export const ADD_USER = gql`
     }
   }
 `;
+
+export const SAVE_BOOK = gql`
+  mutation saveBook($bookData: BookInput!) {
+    saveBook(bookData: $bookData) {
+      _id
+      username
+      email
+      bookCount
+      savedBooks {
+        authors
+        description
+        bookId
+        image
+        link
+        title
+      }
+    }
+  }
+`;
