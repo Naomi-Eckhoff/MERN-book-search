e0690188cf7f8f9a26befd56547470a832daee9b
Naomi-Eckhoff <nsuibhne@gmail.com>
2022-02-20 07:35:12 -0600

    added remove book

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -15,7 +15,7 @@ export const LOGIN_USER = gql`
 
 export const ADD_USER = gql`
   mutation createUser($username:String!, $email:String!, $password: String!){
-    addUser(username: $username, email:$email, password:$password) {
+    createUser(username: $username, email:$email, password:$password) {
       token
       user {
         _id
@@ -29,6 +29,25 @@ export const ADD_USER = gql`
 export const SAVE_BOOK = gql`
   mutation saveBook($bookData: BookInput!) {
     saveBook(bookData: $bookData) {
+      _id
+      username
+      email
+      bookCount
+      savedBooks {
+        authors
+        description
+        bookId
+        image
+        link
+        title
+      }
+    }
+  }
+`;
+
+export const REMOVE_BOOK = gql`
+  mutation deleteBook($bookId: ID!) {
+    deleteBook(bookId: $bookId) {
       _id
       username
       email
